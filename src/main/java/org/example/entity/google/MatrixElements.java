efad3fcd67c410886b799966c4f9e280ec67c19f
Aspire5_Stroganow <111sergey90@gmail.com>
2025-06-05 00:08:51 -0400

    Restore from shelves

--- src/main/java/org/example/entity/google/MatrixElements.java
+++ src/main/java/org/example/entity/google/MatrixElements.java
@@ -1,6 +1,7 @@
 package org.example.entity.google;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.maps.model.Distance;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,4 +11,16 @@ import lombok.Setter;
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MatrixElements {
+    private Distance distance;    // Информация о расстоянии
+    private Duration duration;   // Информация о времени в пути
+    private String status;       // Статус для конкретной пары адресов
+
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Duration {
+        private String text;      // Текстовое представление времени
+        private int value;        // Время в секундах
+    }
 }
